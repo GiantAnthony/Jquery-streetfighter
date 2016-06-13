c0f4d0e82457447154107e529af0bca948b7a64e
GiantAnthony <anthony.espinofgc@gmail.com>
2016-06-13 12:56:20 -0500

    Js Final Commit

--- js/app.js
+++ js/app.js
@@ -1,34 +1,60 @@
+
 // When mouse enters the image area ryu moves //
 $(document).ready(function(){
   $('.ryu').mouseenter(function(){
-    $('.ryu-still').hide();
-    $('.ryu-ready').show();
-  })
+      $('.ryu-still').hide();
+      $('.ryu-ready').show();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').hide();
+  });
 //when mouse enter the image area ryu is still no movement //
-  .mouseleave(function() {
-    $('.ryu-still').show();
-    $('.ryu-ready').hide();
+ $('.ryu').mouseleave(function() {
+      $('.ryu-still').show();
+      $('.ryu-ready').hide();
+      $('.ryu-throwing').hide();
+      $('.ryu-cool').hide();
   })
-  //when mouse is pressed down on the image ryu begin to throw his firebal//
+  //when mouse is pressed down on the image ryu begin to throw his fireball//
   .mousedown(function(){
     playHadouken();
-    $('.ryu-ready').hide();
-    $('.ryu-throwing').show();
+       $('.ryu-ready').hide();
+       $('.ryu-throwing').show();
+       $('.ryu-cool').hide();
+       $('.ryu-still').hide();
     //this is the how far ryu throws his fireball 
-    $('.hadouken').finish().show().animate(
-      {'left': '1020px'},
-      500,
-      function(){
-        $(this).hide();
-        $(this).css('left', '520px');
+      $('.hadouken').finish().show().animate(
+       {'left': '1020px'},
+        500,
+          function(){
+          $(this).hide();
+          $(this).css('left', '520px');
       });
     })
   // when the the mouse is not pressed down image stay still //
   .mouseup(function(){
-    $('.ryu-throwing').hide();
-    $('.ryu-ready').show();
+      $('.ryu-throwing').hide();
+      $('.ryu-ready').show();
+      $('.ryu-still').hide();
+      $('.ryu-cool').hide();
   });
+    $('body').keydown(function(){
+      if (event.keyCode == 88){
+       $('.ryu-cool').show();
+       $('.ryu-ready').hide();
+       $('.ryu-throwing').hide();
+       $('.ryu-still').hide();
+      }
+    });
+    $('body').keyup(function(){
+      if (event.keyCode == 88){
+       $('.ryu-cool').hide();
+       $('.ryu-ready').hide();
+       $('.ryu-throwing').hide();
+       $('.ryu-still').show();
+      }
+    });
 });
+
 
 // music for //
 
@@ -37,3 +63,23 @@ function playHadouken () {
       $('#hadouken-sound') [0].load();
       $('#hadouken-sound') [0].play();
 }
+
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
